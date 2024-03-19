fccab5b73e869c7cc77917c27875196c9927a305
dlamoot <dlamoot@outlook.fr>
2024-03-19 13:56:04 +0100

    MAJ

--- test.c
+++ test.c
@@ -1,3 +1,15 @@
 int void int void void
-
-
+int var_a  = 9;
+9 > 19
+9 + 8
+1 == 7
+"chaine99\n"
+true
+false
+//abcpdfe9902
+if
+else
+for
+then
+1 () " " {} ! = ^ ~ % != >>> << >= <= < > >>
+0x1293
